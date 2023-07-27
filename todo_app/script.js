969a5e33ca6309cd9e9fedd744bf2a6c40b98a45
PushpendraKPal <kumarpushp1992@gmail.com>
2023-07-27 22:44:12 +0530

    added search functionality

--- todo_app/script.js
+++ todo_app/script.js
@@ -5,63 +5,74 @@ var filter = document.getElementById('filter');
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
-itemList.addEventListener('click', removeItem);
+itemList.addEventListener('click', removeOrEditItem); // Combine remove and edit event handling
 // Filter event
 filter.addEventListener('keyup', filterItems);
 
 // Add item
-function addItem(e){
+function addItem(e) {
   e.preventDefault();
 
-  // Get input value
   var newItem = document.getElementById('item').value;
-
-  // Create new li element
+  var newDesc = document.getElementById('description').value;
   var li = document.createElement('li');
-  // Add class
   li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newItem+" "+newDesc));
 
-  // Create del button element
+  //del button element
   var deleteBtn = document.createElement('button');
-
-  // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-  // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
-
-  // Append button to li
   li.appendChild(deleteBtn);
+
+  //edit button element
+  var editBtn = document.createElement('button');
+  editBtn.className = 'btn btn-primary btn-sm float-right edit';
+  editBtn.appendChild(document.createTextNode('Edit'));
+  li.appendChild(editBtn);
 
   // Append li to list
   itemList.appendChild(li);
 }
 
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
+// Remove or Edit item
+function removeOrEditItem(e) {
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Are You Sure you want to delete this item?')) {
       var li = e.target.parentElement;
       itemList.removeChild(li);
+    }
+  } else if (e.target.classList.contains('edit')) {
+    var li = e.target.parentElement;
+    var currentText = li.firstChild.textContent;
+    var newText = prompt('Edit the item:', currentText);
+    if (newText !== null && newText.trim() !== '') {
+      li.firstChild.textContent = newText;
     }
   }
 }
 
 // Filter Items
-function filterItems(e){
-  // convert text to lowercase
+function filterItems(e) {
+  // Convert text to lowercase
   var text = e.target.value.toLowerCase();
   // Get lis
   var items = itemList.getElementsByTagName('li');
   // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
+  Array.from(items).forEach(function(item) {
+    var itemName = item.firstChild.textContent.toLowerCase();
+    var deleteBtn = item.querySelector('.delete');
+    var editBtn = item.querySelector('.edit');
+
+    // Check if the item text contains the filter text
+    if (itemName.indexOf(text) !== -1) {
       item.style.display = 'block';
+      deleteBtn.style.display = 'inline-block';
+      editBtn.style.display = 'inline-block';
     } else {
       item.style.display = 'none';
+      deleteBtn.style.display = 'none';
+      editBtn.style.display = 'none';
     }
   });
 }
