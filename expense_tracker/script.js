694b261d275dcb42421252af5ae31d4a874dfe44
PushpendraKPal <kumarpushp1992@gmail.com>
2023-08-05 10:53:48 +0530

    change in some localstorage functions

--- expense_tracker/script.js
+++ expense_tracker/script.js
@@ -14,8 +14,6 @@ form.addEventListener("submit", (event)=>{
     document.getElementById("description").value="";
     let category = document.getElementById("category").value;
     document.getElementById("category").selectedIndex = 0;
-    console.log(amount, description, category);
-
     newExpense.amount = amount;
     newExpense.description = description;
     newExpense.category = category;
@@ -38,7 +36,6 @@ function setData(obj){
     }else{
         obj.id = data[data.length-1].id +1
     }
-    console.log(obj);
     data.push(obj);
     localStorage.setItem("expense", JSON.stringify(data));
 }
@@ -50,13 +47,15 @@ function render(){
     head.innerHTML = "<th>#</th><th>Amount</th><th>Category</th><th>Description</th><th>Edit</th><th>Delete</th>";
     list.append(head);
     let data = getData();
-    let i=1;
-    data.map(e=>{
+    if(data !== null){
+        let i=1;
+        data.map(e=>{
         let tr = document.createElement("tr");
         tr.innerHTML=`<td>${i}</td><td>${e.amount}</td><td>${e.category}</td><td>${e.description}</td><td><button type="button" class="btn btn-info edit">Edit</button></td><td><button type="button" class="btn btn-danger delete" onclick="del">Delete</button></td>`;
         list.append(tr);
         i++;
-    })
+        })
+    }
 }
 
 function del(e){
@@ -64,26 +63,43 @@ function del(e){
         if (confirm('Are You Sure you want to delete this item?')) {
             var tr = e.target.parentElement.parentElement;
             var currentObjIndex = +(tr.childNodes[0].textContent)-1;
-            console.log(currentObjIndex);
-            dele(currentObjIndex);
             list.removeChild(tr);
+            dele(currentObjIndex);
         }
       } else if (e.target.classList.contains('edit')) {
             var tr = e.target.parentElement.parentElement;
             var currentText = tr.childNodes[1].textContent;
             var currentObjIndex = +(tr.childNodes[0].textContent)-1;
-            console.log(currentObjIndex);
             var newText = prompt('Edit the item:', currentText);
             if (newText !== null && newText.trim() !== '') {
             tr.childNodes[1].textContent = newText;
+            edited(currentObjIndex, newText);
         }
       }
 }
 
 function dele(index){
-    data = getData();
-    data.filter(e=>{
-        return (e.id !== data[index].id)
+    let data = getData();
+    let data2 = data.filter(e=>{
+        if(e.id === data[index].id){
+            console.log("matched");
+            return false;
+        }else{
+            console.log(e.id);
+            return true;
+        }
+   
     })
-    console.log(data);
+    localStorage.setItem("expense", JSON.stringify(data2));
+    console.log(getData());
+}
+
+function edited(index, amount){
+    let data = getData();
+    data.map(e=>{
+        if(e.id === data[index].id){
+            e.amount = amount;
+        }
+    })
+    localStorage.setItem("expense", JSON.stringify(data));
 }
