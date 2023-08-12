ae3ef70ae46d9a6b33354a3f7e907e506a5e818b
PushpendraKPal <kumarpushp1992@gmail.com>
2023-08-12 18:55:14 +0530

    async await changes in the app

--- axios/todo_application/script.js
+++ axios/todo_application/script.js
@@ -1,108 +1,153 @@
 // AXIOS
 
+const baseURL = "https://crudcrud.com/api/b7e6e79d8ba9412f9716817a76beeede/todo"
+
     // Create New Task
 
-let createTask = (todo)=>{
-    axios
-    .post(`https://crudcrud.com/api/0dc4cd8f86414a018b539cb51f28097b/todo`, todo)
-    .then((res)=>{
-        console.log(res.data);
-        DATA = res.data;
-    })
-    .catch((err)=>{
+async function createTask(data){
+    try{
+        const res = await axios.post(`${baseURL}`, data)
+        return res.data;
+    }
+    catch(err){
         console.log(err);
-    })
+    }
 }
 
     // Read all Tasks
-let getAllTask = ()=>{
-    axios
-    .get('https://crudcrud.com/api/0dc4cd8f86414a018b539cb51f28097b/todo')
-    .then((res)=>{
-        render(res.data);
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-}
+    async function readAllTask(){
+        try{
+            const res = await axios.get(`${baseURL}`)
+            return res.data;
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
 
 
     // Read one Task
 
-    let getOneTask = (id)=>{
-        axios
-        .get(`https://crudcrud.com/api/0dc4cd8f86414a018b539cb51f28097b/todo/${id}`)
-        .then((res)=>{
-            console.log(res.data);
-            DATA = res.data;
-        })
-        .catch((err)=>{
+    async function readOneTask(id){
+        try{
+            const res = await axios.get(`${baseURL}/${id}`)
+            return res.data;
+        }
+        catch(err){
             console.log(err);
-        })
+        }
     }
 
     // Update Task
 
-    let getTask = (id, data)=>{
-        axios
-        .put(`https://crudcrud.com/api/0dc4cd8f86414a018b539cb51f28097b/todo/${id}`, data)
-        .then((res)=>{
-            console.log(res.data);
-            DATA = res.data;
-        })
-        .catch((err)=>{
+    async function updateTask(id, data){
+        try{
+            const res = await axios.put(`${baseURL}/${id}`, data)
+            return res.data;
+        }
+        catch(err){
             console.log(err);
-        })
+        }
     }
 
     // Delete Task
 
-    let deleteTask = (id)=>{
-        axios
-        .delete(`https://crudcrud.com/api/0dc4cd8f86414a018b539cb51f28097b/todo/${id}`)
-        .then((res)=>{
-            console.log(res.data);
-            DATA = res.data;
-        })
-        .catch((err)=>{
+    async function deleteTask(id){
+        try{
+            const res = await axios.delete(`${baseURL}/${id}`)
+            return res.data;
+        }
+        catch(err){
             console.log(err);
-        })
+        }
     }
 
 
-// Render function
+// Function Render
 
-const render = (list)=>{
-    let tr;
-    list.map((ele)=>{
-        tr = document.createElement('tr');
-        tr.innerHTML = `<td>${ele.taskName}</td><td>${ele.taskDescription}</td><td><button class="btn">Edit</button><button class="btn">Done</button><button class="btn">Del</button></td><td class="hide">${ele._id}</td>`;
-            
-        if(ele.taskStatus){
-            let compTable = document.getElementById('completeTableBody');
-            compTable.append(tr);
-        }
-        else{
-            let remainingTable = document.getElementById('reamainingTableBody');
-            remainingTable.append(tr);
-        }
-    })
-};
 
-// Edit, Delete and Update
+async function render(){
+    try{
+        let data = await readAllTask();
 
-const singleTodo = (e)=>{
-    e.preventDefault();
+        let compTable = document.getElementById('completeTableBody');
+        compTable.innerHTML = "";
+        let remainingTable = document.getElementById('reamainingTableBody');
+        remainingTable.innerHTML = "";
 
-    let parent = e.target.parentElement.parentElement;
-    let id = parent.lastChild.innerText;
-    console.log(id);
+        let tr;
+        data.map((ele)=>{
+            tr = document.createElement('tr');
+            tr.innerHTML = `<td>${ele.taskName}</td><td>${ele.taskDescription}</td><td><button class="btn btn-info edit">Done</button><button class="btn btn-danger delete">Del</button></td><td class="hide">${ele._id}</td>`;
+                
+            if(ele.taskStatus){
+                compTable.append(tr);
+            }
+            else{
+                remainingTable.append(tr);
+            }
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 
-// Working Code
+
+// Function Add task
+
+async function addTask(e){
+    e.preventDefault();
+    let task = document.getElementById('taskName').value;
+    let taskDis = document.getElementById('taskDescription').value
+    let a = {
+    taskName : task,
+    taskDescription : taskDis,
+    taskStatus : false
+    }
+    await createTask(a);
+    document.getElementById('taskName').value = "";
+    document.getElementById('taskDescription').value = "";
+    render();
+
+}
+
+
+// Function to handle Delete and change in Status.
+
+async function handleDeleteAndEdit(e){
+    try{
+        e.preventDefault()
+        let parent = e.target.parentElement.parentElement;
+        let id = parent.lastChild.innerText;
+        if(e.target.classList.contains('delete')){
+            const deletedItem = await deleteTask(id)
+            render()
+            return deletedItem.data;
+        }else if(e.target.classList.contains('edit')){
+            let toEdit = await readOneTask(id);
+            toEdit.taskStatus = !toEdit.taskStatus
+            const editedData = await updateTask(id, {taskName:toEdit.taskName, taskDescription:toEdit.taskDescription, taskStatus:toEdit.taskStatus});
+            render()
+            return editedData;
+        }
+
+    }
+    catch(err){
+
+    }
+}
+
+
+// Working Code ----------------------------------------------------------------------------------------------
+
+// add new task
 
 console.log("Hello!");
-getAllTask();
+render();
+
+let form = document.getElementById('taskForm');
+form.addEventListener('submit', addTask);
 
 let edu = document.getElementById('taskDetails');
-edu.addEventListener('click', singleTodo);
+edu.addEventListener('click', handleDeleteAndEdit);
